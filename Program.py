4b0b02ba1061b34917eedd2e6dd80449b91b9717
lukaskierzek <lukas.kierzek@gmail.com>
2023-09-14 17:28:43 +0200

    Refactor code

--- Program.py
+++ Program.py
@@ -1,21 +1,21 @@
-from Application import app
+from Application.app import App
 from Application import app_constans as appc
 
 
 def main():
     while True:
-        app.show_menu()
-        option = app.read_option()
+        App.show_menu()
+        option = App.read_option()
         match option:
             case "1":
-                app.clear_console()
+                App.clear_console()
             case "2":
-                app.clear_console()
+                App.clear_console()
             case "3":
-                print(appc.JAPANESE_SEE_YOU_LATER)
+                print(appc.Strings.JAPANESE_SEE_YOU_LATER)
                 return
             case _:
-                app.notice_message(appc.WRONG_OPTION)
+                App.notice_message(appc.Strings.WRONG_OPTION)
 
 
 if __name__ == '__main__':
