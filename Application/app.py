49ff3233601586053f0196136228223b404b4c1a
lukaskierzek <lukas.kierzek@gmail.com>
2023-09-14 17:28:05 +0200

    Add new static method

--- Application/app.py
+++ Application/app.py
@@ -1,22 +1,27 @@
 from os import system, name
 
-
-def read_option() -> str:
-    return input("Enter option number:\n")
+import Application.app_constans as appc
 
 
-def show_menu() -> None:
-    print(f"======MENU======")
-    print("[1] Hiragana")
-    print("[2] Katakana")
-    print("[3] Exit")
+class App:
 
+    @staticmethod
+    def read_option() -> str:
+        return input(appc.Strings.OPTION_NUMBER)
 
-def clear_console() -> None:
-    system('cls') if name == "nt" else system('clear')
+    @staticmethod
+    def show_menu() -> None:
+        print(f"======= MENU =======")
+        print("[1] Hiragana")
+        print("[2] Katakana")
+        print("[3] Exit")
 
+    @staticmethod
+    def clear_console() -> None:
+        system('cls') if name == "nt" else system('clear')
 
-def notice_message(notice: str) -> None:
-    print(notice)
-    input()
-    clear_console()
+    @staticmethod
+    def notice_message(notice: str) -> None:
+        print(notice)
+        input()
+        App.clear_console()
