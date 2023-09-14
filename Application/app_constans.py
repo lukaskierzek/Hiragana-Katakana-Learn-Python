aa1b8903a5aa89ba7844aa94e9e20b9b17b543e7
lukaskierzek <lukas.kierzek@gmail.com>
2023-09-14 17:28:24 +0200

    Add new constants

--- Application/app_constans.py
+++ Application/app_constans.py
@@ -1,6 +1,17 @@
-# region constans string
-WRONG_OPTION = "Wrong option!\nEnter any key to continue"
-JAPANESE_SEE_YOU_LATER = "行ってきます！"
-# endregion
+from enum import StrEnum, IntEnum
 
 
+class Strings(StrEnum):
+    WRONG_OPTION = "Wrong option!\nEnter any key to continue"
+    AGAIN_NO = "N"
+    AGAIN_YES = "Y"
+    AGAIN_QUESTION = f"Again? [{AGAIN_YES}/{AGAIN_NO}]"
+    OPTION_NUMBER = "Enter option number:\n"
+    # region Japanese
+    JAPANESE_SEE_YOU_LATER = "行ってきます！"
+    JAPANESE_CORRECT_ANSWER = "せいかい！"
+    # endregion
+
+
+class Numbers(IntEnum):
+    MAX_NUMBER_OF_ANSWERS = 4
