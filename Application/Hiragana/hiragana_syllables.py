45f531ff7cdb1a3ec9d7f26ce9e9db165b37fca0
lukaskierzek <lukas.kierzek@gmail.com>
2023-09-18 14:42:01 +0200

    Create test for hiragana gojuuon

---
+++ Application/Hiragana/hiragana_syllables.py
@@ -0,0 +1,19 @@
+from enum import StrEnum
+
+
+class Gojuuon(StrEnum):
+    # region vowels
+    A = "あ"
+    I = "い"
+    U = "う"
+    E = "え"
+    O = "お"
+    N = "ん"
+    # endregion
+    # region constant K
+    KA = "か"
+    KI = "き"
+    KU = "く"
+    KE = "け"
+    KO = "こ"
+    # endregion
---
+++ Tests/test_hiragana.py
@@ -0,0 +1,79 @@
+import pytest
+from Application.Hiragana.hiragana_syllables import Gojuuon as hg
+
+
+@pytest.mark.parametrize(
+    "excepted_syllable, actual_syllable",
+    [
+        # region syllables vowels
+        ("あ", hg.A),
+        ("い", hg.I),
+        ("う", hg.U),
+        ("え", hg.E),
+        ("お", hg.O),
+        ("ん", hg.N),
+        # endregion
+        # region constant K
+        ("か", hg.KA),
+        ("き", hg.KI),
+        ("く", hg.KU),
+        ("け", hg.KE),
+        ("こ", hg.KO),
+        # endregion
+        # region constant S
+        ("さ", hg.SA),
+        ("し", hg.SI),
+        ("す", hg.SU),
+        ("せ", hg.SE),
+        ("そ", hg.SO),
+        # endregion
+        # region constant T
+        ("た", hg.TA),
+        ("ち", hg.CHI),
+        ("つ", hg.TSU),
+        ("て", hg.TE),
+        ("と", hg.TO),
+        # endregion
+        # region constant N
+        ("な", hg.NA),
+        ("に", hg.NI),
+        ("ぬ", hg.NU),
+        ("ね", hg.NE),
+        ("の", hg.NO),
+        # endregion
+        # region constant H
+        ("は", hg.HA),
+        ("ひ", hg.HI),
+        ("ふ", hg.FU),
+        ("へ", hg.HE),
+        ("ほ", hg.HO),
+        # endregion
+        # region constant M
+        ("は", hg.HA),
+        ("ひ", hg.HI),
+        ("ふ", hg.FU),
+        ("へ", hg.HE),
+        ("ほ", hg.HO),
+        # endregion
+        # region constant Y
+        ("や", hg.YA),
+        ("ゆ", hg.YU),
+        ("よ", hg.YO),
+        # endregion
+        # region constant R
+        ("ら", hg.RA),
+        ("り", hg.RI),
+        ("る", hg.RU),
+        ("れ", hg.RE),
+        ("ろ", hg.RO),
+        # endregion
+        # region constant W
+        ("わ", hg.WA),
+        ("ゐ", hg.WI),
+        ("ゑ", hg.WE),
+        ("を", hg.WO),
+        # endregion
+    ]
+)
+def test_hiragana_gojuuon(excepted_syllable: str, actual_syllable: str):
+    assert excepted_syllable == actual_syllable
