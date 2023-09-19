e6fee04eae5cbd0189cecefbecac30063d4b0830
lukaskierzek <lukas.kierzek@gmail.com>
2023-09-19 15:37:39 +0200

    Create tests for katakana gojuuon and create katakana syllables

---
+++ Application/Katakana/katakana_syllables.py
@@ -0,0 +1,72 @@
+from enum import StrEnum
+
+
+class Gojuuon(StrEnum):
+    # region vowels
+    A = "ア"
+    I = "イ"
+    U = "ウ"
+    E = "エ"
+    O = "オ"
+    N = "ン"
+    # endregion
+    # region constant K
+    KA = "カ"
+    KI = "キ"
+    KU = "ク"
+    KE = "ケ"
+    KO = "コ"
+    # endregion
+    # region constant S
+    SA = "サ"
+    SHI = "シ"
+    SU = "ス"
+    SE = "セ"
+    SO = "ソ"
+    # endregion
+    # region constant T
+    TA = "タ"
+    CHI = "チ"
+    TSU = "ツ"
+    TE = "テ"
+    TO = "ト"
+    # endregion
+    # region constant N
+    NA = "ナ"
+    NI = "ニ"
+    NU = "ヌ"
+    NE = "ネ"
+    NO = "ノ"
+    # endregion
+    # region constant H
+    HA = "ハ"
+    HI = "ヒ"
+    FU = "フ"
+    HE = "ヘ"
+    HO = "ホ"
+    # endregion
+    # region constant M
+    MA = "マ"
+    MI = "ミ"
+    MU = "ム"
+    ME = "メ"
+    MO = "モ"
+    # endregion
+    # region constant Y
+    YA = "ヤ"
+    YU = "ユ"
+    YO = "ヨ"
+    # endregion
+    # region constant R
+    RA = "ラ"
+    RI = "リ"
+    RU = "ル"
+    RE = "レ"
+    RO = "ロ"
+    # endregion
+    # region constant W
+    WA = "ワ"
+    WI = "ヰ"
+    WE = "ヱ"
+    WO = "ヲ"
+    # endregion
---
+++ Tests/test_katakana.py
@@ -0,0 +1,108 @@
+import pytest
+from Application.Katakana.katakana_syllables import Gojuuon as kg
+
+
+@pytest.fixture
+def number_of_vowels_of_katakana_gojuuon():
+    vowels_of_katakana_gojuuon: list[str] = [kg.A, kg.I, kg.U, kg.E, kg.O, kg.N]
+    return len(vowels_of_katakana_gojuuon)
+
+
+@pytest.fixture
+def number_of_katakana_gojuuon():
+    return len(kg)
+
+
+@pytest.mark.parametrize(
+    "excepted_katakana_syllable, actual_katakana_syllable",
+    [
+        # region syllables vowels
+        ("ア", kg.A),
+        ("イ", kg.I),
+        ("ウ", kg.U),
+        ("エ", kg.E),
+        ("オ", kg.O),
+        ("ン", kg.N),
+        # endregion
+        # region constant K
+        ("カ", kg.KA),
+        ("キ", kg.KI),
+        ("ク", kg.KU),
+        ("ケ", kg.KE),
+        ("コ", kg.KO),
+        # endregion
+        # region constant S
+        ("サ", kg.SA),
+        ("シ", kg.SHI),
+        ("ス", kg.SU),
+        ("セ", kg.SE),
+        ("ソ", kg.SO),
+        # endregion
+        # region constant T
+        ("タ", kg.TA),
+        ("チ", kg.CHI),
+        ("ツ", kg.TSU),
+        ("テ", kg.TE),
+        ("ト", kg.TO),
+        # endregion
+        # region constant N
+        ("ナ", kg.NA),
+        ("ニ", kg.NI),
+        ("ヌ", kg.NU),
+        ("ネ", kg.NE),
+        ("ノ", kg.NO),
+        # endregion
+        # region constant H
+        ("ハ", kg.HA),
+        ("ヒ", kg.HI),
+        ("フ", kg.FU),
+        ("ヘ", kg.HE),
+        ("ホ", kg.HO),
+        # endregion
+        # region constant M
+        ("マ", kg.MA),
+        ("ミ", kg.MI),
+        ("ム", kg.MU),
+        ("メ", kg.ME),
+        ("モ", kg.MO),
+        # endregion
+        # region constant Y
+        ("ヤ", kg.YA),
+        ("ユ", kg.YU),
+        ("ヨ", kg.YO),
+        # endregion
+        # region constant R
+        ("ラ", kg.RA),
+        ("リ", kg.RI),
+        ("ル", kg.RU),
+        ("レ", kg.RE),
+        ("ロ", kg.RO),
+        # endregion
+        # region constant W
+        ("ワ", kg.WA),
+        ("ヰ", kg.WI),
+        ("ヱ", kg.WE),
+        ("ヲ", kg.WO),
+        # endregion
+    ]
+)
+def test_katakana_gojuuon(excepted_katakana_syllable: str, actual_katakana_syllable: str):
+    assert excepted_katakana_syllable == actual_katakana_syllable
+
+
+def test_number_of_katakana_gojuuon_is_48(number_of_katakana_gojuuon):
+    expected_number_of_katakana_gojuuon: int = 48
+    actual_number_of_katakana_gojuuon: int = number_of_katakana_gojuuon
+    assert expected_number_of_katakana_gojuuon == actual_number_of_katakana_gojuuon
+
+
+def test_number_of_vowels_of_katakana_gojuuon_is_6(number_of_vowels_of_katakana_gojuuon):
+    expected_number_of_vowels_of_katakana_gojuuon: int = 6
+    actual_number_of_vowels_of_katakana_gojuuon: int = number_of_vowels_of_katakana_gojuuon
+    assert expected_number_of_vowels_of_katakana_gojuuon == actual_number_of_vowels_of_katakana_gojuuon
+
+
+def test_number_of_contants_of_katakana_gojuuon_is_42(number_of_katakana_gojuuon, number_of_vowels_of_katakana_gojuuon):
+    expected_number_of_contants_of_katakana_gojuuon: int = 42
+    actual_number_of_contants_of_katakana_gojuuon: int = number_of_katakana_gojuuon - number_of_vowels_of_katakana_gojuuon
+    assert expected_number_of_contants_of_katakana_gojuuon == actual_number_of_contants_of_katakana_gojuuon
