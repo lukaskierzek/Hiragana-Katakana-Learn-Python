2a1fc3d6ae0bcc96158c64e2bb81f605d108b0a1
lukaskierzek <lukas.kierzek@gmail.com>
2023-09-20 14:00:44 +0200

    Add new method to syllabary.py

--- Application/syllabary.py
+++ Application/syllabary.py
@@ -5,5 +5,27 @@ class Syllabary(ABC):
 
     @staticmethod
     @abstractmethod
-    def syllabary_gojuuon_list():
+    def syllabary_gojuuon_list() -> list[str]:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def random_syllable() -> str:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def not_the_same_syllable_from_list(syllable_list: list[str]) -> str:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def learn_syllabary() -> None:
+        pass
+
+    @staticmethod
+    @abstractmethod
+    def question_syllabary_gojuuon(answer_list: list[str],
+                                   syllabary_gojuuon_list: list[str],
+                                   syllable_correct_answer: list[str]) -> None:
         pass
